0f3ded46b2b363cf72e313b399506b35c564a0e2
Alfie1456 <161382522+Alfie1456@users.noreply.github.com>
2024-04-08 09:10:35 +1200

    Update racingGame.js
    Collision is working

--- racingGame.js
+++ racingGame.js
@@ -5,31 +5,47 @@
 /*******************************************************/
 console.log("%c t01_create_sprite", "color: blue;");
 
-const screenWidth = 500;
-const screenHeight = 500;
-const obstacleHeight = 25;
-const obstacleWidth = 25;
+const SCREENWIDTH = 500;
+const SCREENHEIGHT = 500;
 
 
-var screenSelector = "start";  
+
+var screenSelector = "start";
 var score = 0;
 var nextSpawn = 0;
-var player,playerImage;
-var road,roadImage;
-var carOb1Img,carOb1;
-var bikeOb1Img,bikeOb1;
-var truckOb1Img,truckOb1;
-var carOb1Group,truckOb1;
-var bikeOb1Group,truckOb1;
-var bikeOb1Group,carOb1;
+var playerImage;
+var roadImage;
+var carOb1;
+var player;
+var truckOb1;
+var carOb1Group, truckOb1Group;
+var lifeTime = 10;
 
-var lifeTime = 1;
 function preload() {
+    //images
     roadImage = loadImage('/Assets/images/road.png');
     playerImage = loadImage('/Assets/images/player.png');
     carOb1Img = loadImage('/Assets/images/carObstacle1.png');
     truckOb1Img = loadImage('/Assets/images/truckObstacle1.png');
-    bikeOb1Img = loadImage('/Assets/images/bikeObstacle1.png');
+
+    //truck spawner
+    truckOb1 = createSprite(100, 100, 30, 30, 'd');
+    truckOb1.addImage(truckOb1Img);
+    truckOb1.scale = 0.7;
+    truckOb1.x = Math.round(random(350, 500));
+    truckOb1.lifetime = "lifetime";
+    
+    //car spawner
+    carOb1 = createSprite(100, 250, 30, 30, 'd');
+    carOb1.addImage(carOb1Img);
+    carOb1.scale = 0.6;
+    carOb1.x = Math.round(random(400, 500));
+    carOb1.lifetime = "lifeTime";
+
+    //player spawner 
+    player = new Sprite(100, 100, 100, 100);
+    player.addImage(playerImage);
+    player.scale = 0.48;
 
 }
 
@@ -37,91 +53,82 @@ function preload() {
 // setup()
 /*******************************************************/
 function setup() {
-    
-    cnv = new Canvas (screenWidth, screenHeight);
 
-    obstacles = new Group();
-    
-        //screen selecter 
-    document.addEventListener("keydown", 
+    cnv = new Canvas(SCREENWIDTH, SCREENHEIGHT);
+
+
+    //keypress listener 
+    document.addEventListener("keydown",
         function(event) {
-            if(screenSelector == "start"||screenSelector == "end" ||screenSelector == "instructions"){
+            if (screenSelector == "start" || screenSelector == "end") {
                 screenSelector = "game"
                 resetGame();
-            
-            
+
+
             }
-    });
-    
- 
-    
-    carOb1Group= new Group();
-    truckOb1Group= new Group();
-    bikeOb1Group= new Group();
+        });
+
+    //walls/borders
+    wallLH = new Sprite(0, height / 2, 8, height, 'k');
+    wallLH.color = 'black';
+    wallTop = new Sprite(0, 0, windowWidth * 2, 8, 'k');
+    wallTop.color = 'black';
+    wallBot = new Sprite(0, height, windowWidth * 2, 15, 'k');
+    wallBot.color = 'black';
+
+
+
 }
 
 /*******************************************************/
 // draw()
 /*******************************************************/
-        //loop
+//loop
 function draw() {
-    if(screenSelector=="game"){
+    
+    //screen selector
+    chooseScreen(screenSelector);
+
+    //colision with sprites
+    if (truckOb1.colliding(player)) {
+
+        screenSelector = "end"
+        console.log("coliding")
+
+    } else if (carOb1.colliding(player)) {
+
+        screenSelector = "end"
+        console.log("coliding")
+    }
+    //colision with wall 
+    if (truckOb1.colliding(wallLH)) {
+        truckOb1.x = 700;
+    }
+
+    if (carOb1.colliding(wallLH)) {
+        carOb1.x = 800;
+    }
+
+
+}
+
+//screen selector
+function chooseScreen(screenType){
+    if (screenType == "game") {
         gameScreen();
-    }else if(screenSelector=="end"){
+    } else if (screenType == "end") {
         endScreen();
-    }else if(screenSelector=="start"){
+    } else if (screenType == "start") {
         startScreen();
-    }else{
+    } else {
         text("not the right screen. Try restarting the game.", 50, 50);
         console.log("not the right screen. Try resetting.")
-    }
+    } 
 }
 
-function spawncarOb1(){
-    if(frameCount%250===0){
-    var carOb1=createSprite( 100,250,30,30);
-    carOb1.addImage(carOb1Img);
-    carOb1.scale=0.4;
-    carOb1.vel.x=-4;
-    carOb1.x=Math.round(random(450,500));
-    carOb1.lifetime="lifeTime";
- }
-}
 
-        //Truck spawner
-function spawntruckOb1(){
-    if(frameCount%350===0){
-    var truckOb1=createSprite(100,100,30,30);
-    truckOb1.addImage(truckOb1Img);
-    truckOb1.scale=0.4;
-    truckOb1.vel.x=-5;
-    truckOb1.x=Math.round(random(400,500));
-    truckOb1.lifetime="lifetime";
- }
-}
- 
-         //Truck spawner
-function spawnbikeOb1(){
-    if(frameCount%350===0){
-    var bikeOb1=createSprite(100,350,30,30);
-    bikeOb1.addImage(bikeOb1Img);
-    bikeOb1.scale=0.4;
-    bikeOb1.vel.x=-5;
-    bikeOb1.x=Math.round(random(400,500));
-    bikeOb1.lifetime="lifetime";
- }
-} 
-       
-
-        //colision detecter
-function youDead(_player, _truckOb1Img) {
-    screenSelector ="end";
-    player.remove();
-    truckOb1.removeAll();
-}
-
-        //Start screen
-function startScreen(){
+//Start screen
+function startScreen() {
     background(roadImage);
 
     allSprites.visible = false;
@@ -133,43 +140,42 @@ function startScreen(){
     textSize(24);
     text("Press any key to start", 50, 110);
 }
-        
-        //game screen
-function gameScreen(){
+
+//game screen
+function gameScreen() {
     background(roadImage);
-    
+
     allSprites.visible = true;
-    score++;
+
+    if (screenSelector == "game") {
+        score++;
+    }
+
     player.speed = 3;
-    
-    
-	
-	if (kb.pressing('up')) {
-		player.direction = -90;
-	} else if (kb.pressing('down')) {
-		player.direction = 90;
-	} else if (kb.pressing('left')) {
-		player.direction = 180;
-	} else if (kb.pressing('right')) {
-		player.direction = 0;
-	} else {
-	  player.speed = 0;
-	}
-	
- 
-    
-  
-    spawncarOb1();
-    spawntruckOb1();
-}
+    truckOb1.vel.x = -5;
+    carOb1.vel.x = -4;
+    if (kb.pressing('up')) {
+        player.direction = -90;
+    } else if (kb.pressing('down')) {
+        player.direction = 90;
+    } else if (kb.pressing('left')) {
+        player.direction = 180;
+    } else if (kb.pressing('right')) {
+        player.direction = 0;
+    } else {
+        player.speed = 0;
+    }
+
+
     textSize(32);
     fill(255);
     stroke(0);
     strokeWeight(4);
     text(score, 50, 50);
+}
 
-        //end screen
-function endScreen(){
+//end screen
+function endScreen() {
     background("white");
     background("red");
     allSprites.visible = false;
@@ -179,17 +185,26 @@ function endScreen(){
     strokeWeight(4);
     text("you died. ", 50, 50);
     textSize(24);
-    text("your score was: "+score, 50, 110);
+    text("your score was: " + score, 50, 110);
     textSize(14);
     text("press any key to restart", 50, 150);
-   
+    carOb1.vel.x = 0;
+    truckOb1.vel.x = 0;
+    carOb1.vel.y = 0;
+    truckOb1.vel.y = 0;
+    truckOb1.x = 500;
+    carOb1.x = 600;
+    player.x = 100;
+    truckOb1.rotationSpeed = 0;
+    carOb1.rotationSpeed = 0;
+    player.rotationSpeed = 0;
+    truckOb1.rotation = 0;
+    carOb1.rotation = 0;
+    player.rotation = 0;
 }
-        //restart game function 
-function resetGame(){
-       player=new Sprite(100,100,100,100);
-    player.addImage(playerImage);
-    player.scale=0.4;
-    player.collides(truckOb1, youDead);
+
+//score reset 
+function resetGame() {
     score = 0;
 }
 
